6ae1a677fc9c09dfee37fdbe7d3ef817c0270c51
kevin <415612030@qq.com>
2012-10-09 22:27:49 +0800

    Add new mvc solution  for  front development

---
+++ PetSolution/Pet/Controllers/PetBussiness/BussinessController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pet.Controllers.PetBussiness
+{
+    public class BussinessController : Controller
+    {
+        //
+        // GET: /Bussiness/
+
+        public JsonResult GetBasicMessage()
+        {
+            return Json("This is a message from BussinessController",JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ PetSolution/Pet/Controllers/User/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pet.Controllers.User
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public JsonResult Index()
+        {
+            return Json("This is a message from UserController",JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
