be5e1617aec3b85fb74d638c8164747482957aac
kevin <415612030@qq.com>
2012-10-16 23:06:58 +0800

    Add mvc proj to PetCare.sln and copy .aspx code to index.html file

---
+++ Pet/html/js/init_index.js
@@ -0,0 +1,25 @@
+﻿define(function (require, exports, module) {
+    var _ = require("underscore");
+    var Backbone = require("backbone");
+    var AppView = Backbone.View.extend({
+        el: $("body"),
+        initialize: function () {
+            $("#test").click(function () {
+                $.get("../Adopt/GetBasicMessage.do", null, function (json) {
+                    $("#adoptext").append(json);
+                });
+            });
+            $("#test").click(function () {
+                $.get("../Bussiness/GetBasicMessage.do", null, function (json) {
+                    $("#bussitext").append(json);
+                });
+            });
+            $("#test").click(function () {
+                $.get("../User/Index.do", null, function (json) {
+                    $("#usertext").append(json);
+                });
+            });
+        }
+    });
+    var App = new AppView();
+});
